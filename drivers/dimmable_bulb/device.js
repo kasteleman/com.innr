41c405d9a60741ef87907e51a40ba7d3c2ef8e2b
RobinBol <robinbolscher@gmail.com>
2017-08-10 18:01:19 +0200

    Update meshdriver, fix drivers, add RB 162

--- drivers/dimmable_bulb/device.js
+++ drivers/dimmable_bulb/device.js
@@ -3,9 +3,10 @@
 const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
 
 class DimmableBulb extends ZigBeeLightDevice {
-  	onMeshInit() {
-      this.printNode();
-    }
+	onMeshInit() {
+		super.onMeshInit();
+		this.printNode();
+	}
 }
 
 module.exports = DimmableBulb;
