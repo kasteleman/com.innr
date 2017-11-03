6a81a2b2b85d84fec073c98a6aa6f67a67d023eb
kasteleman <kasteleman@gmail.com>
2017-11-03 21:43:47 +0100

    fixed e14 icon

--- drivers/tunable_bulb/device.js
+++ drivers/tunable_bulb/device.js
@@ -3,6 +3,35 @@
 const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
 
 class TunableBulb extends ZigBeeLightDevice {
+	onMeshInit() {
+		this.printNode();
+		this.enableDebug();
+
+		if (this.hasCapability('onoff')) {
+			this.registerCapability('onoff', 'genOnOff', {
+				getOpts: {
+					getOnStart: true,
+				},
+			});
+		}
+
+		if (this.hasCapability('dim')) {
+			this.registerCapability('dim', 'genLevelCtrl', {
+				getOpts: {
+					getOnStart: true,
+				},
+			});
+		}
+
+		if (this.hasCapability('light_temperature')) {
+			this.registerCapability('light_temperature', 'lightingColorCtrl', {
+				getOpts: {
+					getOnStart: true,
+				},
+			});
+		}
+
+	}
 
 }
 
