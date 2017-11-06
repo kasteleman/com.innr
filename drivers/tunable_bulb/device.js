5be6730a48d37bee22f5e97dc6d3e17bdaf011c5
kasteleman <kasteleman@gmail.com>
2017-11-06 19:07:28 +0100

    cleanup

--- drivers/tunable_bulb/device.js
+++ drivers/tunable_bulb/device.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// const util = require('/node_modules/homey-meshdriver/lib/util');
-
 const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
 
 class TunableBulb extends ZigBeeLightDevice {
