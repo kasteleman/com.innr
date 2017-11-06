b29b51545d95ea4095ab148b728f3f9acb3cdd08
kasteleman <kasteleman@gmail.com>
2017-11-06 18:59:18 +0100

    E27 adjusted

--- drivers/tunable_bulb/device.js
+++ drivers/tunable_bulb/device.js
@@ -1,37 +1,10 @@
 'use strict';
 
+// const util = require('/node_modules/homey-meshdriver/lib/util');
+
 const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
 
 class TunableBulb extends ZigBeeLightDevice {
-	onMeshInit() {
-		this.printNode();
-		this.enableDebug();
-
-		if (this.hasCapability('onoff')) {
-			this.registerCapability('onoff', 'genOnOff', {
-				getOpts: {
-					getOnStart: true,
-				},
-			});
-		}
-
-		if (this.hasCapability('dim')) {
-			this.registerCapability('dim', 'genLevelCtrl', {
-				getOpts: {
-					getOnStart: true,
-				},
-			});
-		}
-
-		if (this.hasCapability('light_temperature')) {
-			this.registerCapability('light_temperature', 'lightingColorCtrl', {
-				getOpts: {
-					getOnStart: true,
-				},
-			});
-		}
-
-	}
 
 }
 
