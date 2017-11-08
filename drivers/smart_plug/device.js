45cabd15de670ed8d22b0d37c0e3bb4bcc87757d
kasteleman <kasteleman@gmail.com>
2017-11-08 18:29:28 +0100

    fixed smart plug

--- drivers/smart_plug/device.js
+++ drivers/smart_plug/device.js
@@ -4,6 +4,13 @@ const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
 
 class SmartPlug extends ZigBeeDevice {
 	onMeshInit() {
+		if (this.hasCapability('onoff')) {
+			this.registerCapability('onoff', 'genOnOff', {
+				getOpts: {
+					getOnStart: true,
+				},
+			});
+		}
 
 		// Report is send if status is changed or after 5 min
 		this.registerAttrReportListener('genOnOff', 'onOff', 1, 300, 1, data => {
